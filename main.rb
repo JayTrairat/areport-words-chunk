6571bb225997c434a72fa2f186ef3777ca82da07
JayTrairat <JayTrairat@JayTrairats-MacBook-Pro.local>
2018-04-25 16:01:59 +0700

    feat(): stats preparation

--- main.rb
+++ main.rb
@@ -1,10 +1,22 @@
+require 'enumerator'
+
 def main
-  contents = Array.new()
+  contents = Array.new
   File.open('assets/google_sheet.csv', 'r') do |source|
     contents = source.readlines
   end
 
+  (0..27).each do |item|
+    content = contents[item].strip.split('|')
+    content = content.each_slice(3).to_a
 
+    File.open("assets/google_sheet_export_position_#{item + 1}.csv", 'w') do |outp|
+      content.each do |element|
+          outp.write(element)
+          outp.write("\n")
+      end
+    end
+  end
 end
 
 main()
