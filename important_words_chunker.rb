d2b5ec9df184fb8faaa27d4d2f0251b91396b6be
JayTrairat <JayTrairat@JayTrairats-MacBook-Pro.local>
2018-04-26 16:04:56 +0700

    feat(): insert into chunks

---
+++ important_words_chunker.rb
@@ -0,0 +1,19 @@
+require 'enumerator'
+
+def main()
+  contents = Array.new
+
+  File.open("assets/important_words.txt", "r") do |source|
+    contents = source.readlines.map { |content| content.strip }
+  end
+
+  contents = (contents.each_slice((contents.size/4.0).round).to_a)
+
+  (0..contents.size - 1).each do |index|
+    File.open("assets/important_words_pos_#{index + 1}.txt", "w") do |outp|
+      outp.write(contents[index].join("\n"))
+    end
+  end
+end
+
+main()
